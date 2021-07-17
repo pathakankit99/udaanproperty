c36f455adb1f22e7e4d14bd2354fdca244dbc7a2
Ankit Pathak <ankitpathak143192@gmail.com>
2021-07-17 09:31:03 +0530

    .

--- pages/contact.js
+++ pages/contact.js
@@ -9,10 +9,6 @@ function contact() {
                 <title>Udaan Property - Contact Us</title>
                 <meta name="description" content="Generate a SEO Friendly Description" />
                 <link rel="icon" href="/favicon.ico" />
-                <script src="https://cdn.jsdelivr.net/npm/emailjs-com@2.4.0/dist/email.min.js"></script>
-                <script dangerouslySetInnerHTML={{ __html: `(function(){
-                    emailjs.init("%REACT_APP_EMAILJS_USERID%");
-                })();` }} />
             </Head>
             <Navbar active="contact"/>
             <main>
